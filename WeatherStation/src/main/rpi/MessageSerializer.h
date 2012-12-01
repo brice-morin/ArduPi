33f6a3867008211d548fec4422e5dba25634fdbc
brice-morin <brice.morin1@gmail.com>
2012-12-01 17:00:17 +0100

    Weather station

---
+++ WeatherStation/src/main/rpi/MessageSerializer.h
@@ -0,0 +1,57 @@
+/*****************************************************
+ *      THIS IS A GENERATED FILE. DO NOT EDIT.
+ *           Header for Thing MessageSerializer
+ *  Generated from ThingML (http://www.thingml.org)
+ *****************************************************/
+
+#ifndef MessageSerializer_H_
+#define MessageSerializer_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "thingml_typedefs.h"
+
+/*****************************************************************************
+ * Headers for type : MessageSerializer
+ *****************************************************************************/
+
+// Definition of the instance stuct:
+struct MessageSerializer_Instance {
+// Variables for the ID of the instance
+int id;
+// Variables for the current instance state
+int MessageSerializer_SerializerBehavior_State;
+// Variables for the properties of the instance
+uint8_t PacketManager_lengthInteger_var;
+uint8_t PacketManager_lengthString_var;
+uint8_t PacketManager_lengthUInt16_var;
+int PacketManager_MAX_PACKET_SIZE_var;
+uint8_t PacketManager_START_BYTE_var;
+uint8_t PacketManager_STOP_BYTE_var;
+uint8_t PacketManager_ESCAPE_BYTE_var;
+int PacketManager_CODE_POSITION_var;
+int PacketManager_LENGTH_POSITION_var;
+int PacketManager_DATA_POSITION_var;
+uint8_t PacketManager_buffer_var[16];
+int PacketManager_index_var;
+};
+
+// Declaration of prototypes outgoing messages:
+void MessageSerializer_SerializerBehavior_OnEntry(int state, struct MessageSerializer_Instance *_instance);
+void MessageSerializer_handle_RemoteControl_changeDisplay(struct MessageSerializer_Instance *_instance);
+// Declaration of callbacks for incomming messages:
+void register_MessageSerializer_send_serial_serial_tx_listener(void (*_listener)(struct MessageSerializer_Instance*, uint8_t));
+
+// Definition of the states:
+#define MESSAGESERIALIZER_SERIALIZERBEHAVIOR_STATE 0
+#define MESSAGESERIALIZER_SERIALIZERBEHAVIOR_SERIALIZE_STATE 1
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //MessageSerializer_H_
---
+++ WeatherStation/src/main/rpi/WeatherStation.h
@@ -0,0 +1,51 @@
+/*****************************************************
+ *      THIS IS A GENERATED FILE. DO NOT EDIT.
+ *           Header for Thing WeatherStation
+ *  Generated from ThingML (http://www.thingml.org)
+ *****************************************************/
+
+#ifndef WeatherStation_H_
+#define WeatherStation_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "thingml_typedefs.h"
+
+/*****************************************************************************
+ * Headers for type : WeatherStation
+ *****************************************************************************/
+
+// Definition of the instance stuct:
+struct WeatherStation_Instance {
+// Variables for the ID of the instance
+int id;
+// Variables for the current instance state
+int WeatherStation_SensorsDisplayImpl_State;
+// Variables for the properties of the instance
+};
+
+// Declaration of prototypes outgoing messages:
+void WeatherStation_SensorsDisplayImpl_OnEntry(int state, struct WeatherStation_Instance *_instance);
+void WeatherStation_handle_RemoteControlIn_temperature(struct WeatherStation_Instance *_instance, uint16_t temp);
+void WeatherStation_handle_RemoteControlIn_light(struct WeatherStation_Instance *_instance, uint16_t light);
+void WeatherStation_handle_timer_timer_timeout(struct WeatherStation_Instance *_instance);
+void WeatherStation_handle_Start_start(struct WeatherStation_Instance *_instance);
+// Declaration of callbacks for incomming messages:
+void register_WeatherStation_send_RemoteControlOut_changeDisplay_listener(void (*_listener)(struct WeatherStation_Instance*));
+void register_WeatherStation_send_timer_timer_start_listener(void (*_listener)(struct WeatherStation_Instance*, int));
+void register_WeatherStation_send_timer_timer_cancel_listener(void (*_listener)(struct WeatherStation_Instance*));
+
+// Definition of the states:
+#define WEATHERSTATION_SENSORSDISPLAYIMPL_STATE 0
+#define WEATHERSTATION_SENSORSDISPLAYIMPL_INIT_STATE 1
+#define WEATHERSTATION_SENSORSDISPLAYIMPL_PROCESS_STATE 2
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //WeatherStation_H_
